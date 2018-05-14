d2f14f81f02ae6583e7e3118406c6d956f979052
Matt Strimas-Mackey <mstrimas@gmail.com>
2018-05-14 15:32:32 -0400

    issue with special characters in species common names

--- R/read-lifelist.r
+++ R/read-lifelist.r
@@ -3,9 +3,11 @@ read_lifelist <- function(x) {
   if (!"Species" %in% names(ll)) {
     return(NULL)
   }
-  ll <- dplyr::select(ll, common_name = Species)
-  ll <- dplyr::inner_join(ll, auk::ebird_taxonomy, by = "common_name")
-  ll <- dplyr::pull(ll, species_code)
+  ll <- dplyr::select(ll, common_name = Species) %>% 
+    dplyr::mutate(common_name = stringi::stri_trans_general(common_name, 
+                                                            "latin-ascii")) %>% 
+    dplyr::inner_join(auk::ebird_taxonomy, by = "common_name") %>% 
+    dplyr::pull(species_code)
   if (length(ll) == 0) {
     return(NULL)
   } else {
